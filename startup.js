998ebbd4798a33124b761a753dd2fcb460e063a2
stevewooo <461437874@qq.com>
2018-12-29 21:14:35 +0800

    https server

--- startup.js
+++ startup.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('/sslkeys/private.pem', 'utf8'),
+var certificate = fs.readFileSync('/sslkeys/file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+var PORT = 80;
+var SSLPORT = 443;
+
+httpServer.listen(PORT, function() {
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+httpsServer.listen(SSLPORT, function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
 app.use("/public", express.static("public"));
-
-app.listen(80);
+// Welcome
+app.get('/api', function(req, res) {
+    if(req.protocol === 'https') {
+        res.status(200).send('Welcome to Safety Land!');
+    }
+    else {
+        res.status(200).send('Welcome!');
+    }
+});
